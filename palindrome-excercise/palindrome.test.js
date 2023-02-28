17c1ba4059338114c817cfe800e68d98e8a8965a
AbdlMub <mubh1nabdul@hotmail.co.uk>
2023-02-28 11:58:34 +0000

    Completed excercise 3

--- palindrome-excercise/palindrome.test.js
+++ palindrome-excercise/palindrome.test.js
@@ -1,3 +1,20 @@
 const check_Palindrome = require('./palindrome')
 
-describe
+describe("basic tests", () => {
+    it("should be a palindrome", () => {
+        const result = check_Palindrome('madam');
+        expect(result).toEqual(true)
+    })
+
+    it("should be non-palindrome", () => {
+        const result = check_Palindrome('fox');
+        expect(result).toEqual(false)
+    })
+
+    it("should be a palindrome with 2 words", () => {
+        const result = check_Palindrome('nurses run');
+        expect(result).toEqual(true)
+    })
+
+
+})
